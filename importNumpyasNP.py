87183acba023cfa9ee96a3ccc92ceea0ca9d9bae
Desfita natasya rosdiana siburian simatupang <150890662+pita2112@users.noreply.github.com>
2024-09-19 22:04:55 +0800

    Create importNumpyasNP.py

---
+++ importNumpyasNP.py
@@ -0,0 +1,21 @@
+# Mendefinisikan fungsi f(x) = x + e^x
+def f(x):
+    return x + np.exp(x)
+
+# Metode Biseksi
+def biseksi(f, a, b, toleransi=1e-6, max_iterasi=10):
+    if f(a) * f(b) > 0:
+        print("Fungsi tidak memiliki akar.")
+        return None
+    
+    iterasi_count = 0
+    while (b - a) / 2 > toleransi and iterasi_count < max_iterasi:
+        c = (a + b) / 2
+        if f(c) == 0:  # Jika nilai tengah adalah akar
+            return c
+        elif f(a) * f(c) < 0:
+            b = c
+        else:
+            a = c
+        iterasi_count += 1
+    return (a + b) / 2
